e8a41b64ab012664e2f26f675ddd672a1b883d6d
Andrei Spanu <andrei.spanu@orcha.co.uk>
2024-10-01 08:50:43 +0100

    Additional Test

--- src/App.cy.tsx
+++ src/App.cy.tsx
@@ -1,9 +1,15 @@
 import React from 'react'
 import App from './App'
+import { mount } from 'cypress/react'
 
 describe('<App />', () => {
   it('renders', () => {
     // see: https://on.cypress.io/mounting-react
-    cy.mount(<App />)
+    mount(<App />)
+  })
+
+  it('renders learn react link', () => {
+    mount(<App />)
+    cy.contains('Learn React').should('be.visible')
   })
 })
